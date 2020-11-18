74e7b2a9e6c3f49b31c370a104ad4c5194878e6a
codeclanstudent <sstewart1993@outlook.com>
2020-11-18 11:42:05 +0000

    ur turn

---
+++ src/main/java/Coin.java
@@ -0,0 +1,16 @@
+public class Coin {
+
+    CoinType coinType;
+
+    public Coin(CoinType coinType) {
+        this.coinType = coinType;
+    }
+
+    public CoinType getCoinType() {
+        return coinType;
+    }
+    
+    public double getCoinValue(){
+        return coinType.getValue();
+    }
+}
