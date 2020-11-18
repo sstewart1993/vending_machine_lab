c7f1f0d2ad99a5f6d7c94465cadb386f932aa876
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-11-18 15:09:19 +0000

    finished task 13

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -12,5 +12,26 @@ public class VendingMachine {
         drawers = new ArrayList<Drawer>();
     }
 
+    public CoinReturn getCoinReturn() {
+        return coinReturn;
+    }
 
+    public ArrayList<Coin> getCoins() {
+        return coins;
+    }
+
+    public ArrayList<Drawer> getDrawers() {
+        return drawers;
+    }
+
+    public void addCoinToMachine(Coin coin) {
+        this.coins.add(coin);
+    }
+
+    public boolean coinCheck(Coin coin) {
+        if(coin.getCoinValue() < 0.05) {
+            return false;
+        }
+        return true;
+    }
 }
